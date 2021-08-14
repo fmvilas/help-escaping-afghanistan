37a5994c0452b660d862932b184fad458efd0151
Fran Mendez <fmvilas@gmail.com>
2021-08-14 15:26:50 +0200

    Fix Next.js issues

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import Script from 'next/script'
 import Card from './components/Card'
 
 const tiers = {
@@ -14,11 +15,11 @@ const tiers = {
 }
 
 export default function Home() {
-  if (typeof window === 'undefined') return null
   const [tier, setTier] = useState(tiers['price_1JOKBNCDK8etNbTKMW1r9KIx'])
   const [error, setError] = useState()
 
   const redirectToCheckout = (priceId) => {
+    if (typeof window === 'undefined') return null
     const stripe = window.Stripe('pk_test_51JOJnNCDK8etNbTKBTHsOKvmhHu68XQU9FzFcPnHgMe2DPEcm6N8U1UWnk0FZFYkDEnUfzdNL9byq25eH8Cnu8b900ZTz8RXvj')
 
     stripe.redirectToCheckout({
@@ -45,20 +46,12 @@ export default function Home() {
       })
   }
 
-  const onClickDonate25 = () => {
-    redirectToCheckout('price_1JOKBNCDK8etNbTKMW1r9KIx')
-  }
-
-  const onClickDonate100 = () => {
-    redirectToCheckout('price_1JOKBxCDK8etNbTKhRLmD5F1')
-  }
-
   return (
     <div className="bg-white">
       <Head>
         <title>Help escaping Afghanistan</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src="https://js.stripe.com/v3"></script>
+        <Script src="https://js.stripe.com/v3"></Script>
       </Head>
       {error && (<div>{error}</div>)}
 
@@ -72,13 +65,13 @@ export default function Home() {
           <div>
             <div className="prose lg:prose-xl">
               <p>
-                Hi! This is Lukasz and Fran on behalf of our friend. We're trying to fundraise for him and his family to escape from the war in Afghanistan. This is his story:
+                Hi! This is Lukasz and Fran on behalf of our friend. We&apos;re trying to fundraise for him and his family to escape from the war in Afghanistan. This is his story:
               </p>
               <p>
                 ---
               </p>
               <p>
-                I am a programmer from Herat, Afghanistan. I can't disclose my name for the reasons that I will explain later. I was fortunate enough to get a scholarship and graduate from Pune University. When I lived in India to complete my bachelor's degree, the Taliban were expanding their territory over Afghanistan. I heard a lot of horror stories from my family and relatives that were living in Afghanistan. 6 Months ago, I graduated and came back to Herat, Afghanistan. Although my family members were not happy with my decision by coming back, but I couldn't leave my family alone to the horror that was coming for them.
+                I am a programmer from Herat, Afghanistan. I can&apos;t disclose my name for the reasons that I will explain later. I was fortunate enough to get a scholarship and graduate from Pune University. When I lived in India to complete my bachelor&apos;s degree, the Taliban were expanding their territory over Afghanistan. I heard a lot of horror stories from my family and relatives that were living in Afghanistan. 6 Months ago, I graduated and came back to Herat, Afghanistan. Although my family members were not happy with my decision by coming back, but I couldn&apos;t leave my family alone to the horror that was coming for them.
               </p>
               <p>
                 After coming back to Afghanistan, I started contributing to open source. It was a go-to relief for me In the hard days of the war. To get involved in a community that was eager to help each other and as a result, create something beautiful for everyone. It just helped me forget the terrors that my relatives were enduring under the Taliban regime and the fact that each day they are closing in.
@@ -87,7 +80,7 @@ export default function Home() {
                 In the past few days when the war was at its peak, it was hard to see my 4 years old sister cry to sleep while hearing the most horrific explosions and gun fires around her.
               </p>
               <p>
-                By the time that I am writing this, Taliban took over the city, and escaping Afghanistan became impossible for us because it's hard to escape a country when you are barely above the poverty line. Since the open-source contribution is not a common thing, My family and I are one "Hey, this guy worked for non-believers of Islam" away from getting killed or tortured. They killed people for less than that. You can never explain open-source to someone who hasn't read a single line of text in his entire life. And this is the reason that I'm not disclosing my name here.
+                By the time that I am writing this, Taliban took over the city, and escaping Afghanistan became impossible for us because it&apos;s hard to escape a country when you are barely above the poverty line. Since the open-source contribution is not a common thing, My family and I are one &quot;Hey, this guy worked for non-believers of Islam&quot; away from getting killed or tortured. They killed people for less than that. You can never explain open-source to someone who hasn&apos;t read a single line of text in his entire life. And this is the reason that I&apos;m not disclosing my name here.
               </p>
               <p>
                 All of the foreign organizations have evacuated their employees and people who have helped them in Afghanistan because in their view, working for non-believers of Islam is the biggest sin that you can commit.
